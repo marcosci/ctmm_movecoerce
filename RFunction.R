368243c496cc9d3b56220cbeea627946e0dc0319
Marco Sciaini <marco@kaldera.dev>
2023-03-23 16:36:57 +0100

    first commit

---
+++ RFunction.R
@@ -0,0 +1,11 @@
+library('move')
+library('ctmm')
+
+## The parameter "data" is reserved for the data object passed on from the previous app
+
+## to display messages to the user in the log file of the App in MoveApps one can use the function from the logger.R file: 
+# logger.fatal(), logger.error(), logger.warn(), logger.info(), logger.debug(), logger.trace()
+
+rFunction = function(data, year) {
+  as.telemetry(data)
+}
